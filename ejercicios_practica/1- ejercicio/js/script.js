58595c845249ac6f8dcf506fa787e5f19f049d8e
CAVega2705 <94246170+CAVega2705@users.noreply.github.com>
2022-07-12 17:10:08 -0300

    Ejercicios terminado

--- ejercicios_practica/1- ejercicio/js/script.js
+++ ejercicios_practica/1- ejercicio/js/script.js
@@ -9,8 +9,12 @@
 
 1)
 Leer el título del "header" del post e imprimirlo en consola.   
-
+*/
+const tituloHeader = document.querySelector (".post-header")
+console.log(tituloHeader.textContent)
+/*
 2)
 Cambiar título utilizando javascript por otro cualquiera.
-
 */
+tituloHeader.textContent = `Nuevo Título`
+console.log(tituloHeader.textContent)
