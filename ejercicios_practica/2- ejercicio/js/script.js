aaad1ed91504291b32f4cde3b4cfde9bacc5c94c
CAVega2705 <94246170+CAVega2705@users.noreply.github.com>
2022-07-12 17:10:53 -0300

    Ejercicios terminado

--- ejercicios_practica/2- ejercicio/js/script.js
+++ ejercicios_practica/2- ejercicio/js/script.js
@@ -11,9 +11,17 @@
 Obtener cada uno de los botones utilizando su ID,
 luego cambiar el texto de los botones por:
 PEGAR / COPIAR / CORTAR
+*/
+document.getElementById(`boton1`).innerHTML = `PEGAR`
+document.getElementById(`boton2`).innerHTML = `COPIAR`
+document.getElementById(`boton3`).innerHTML = `CORTAR`
 
+/*
 2)
 Obtener todos los botones utilizando la clase "btn",
 luego a todos los botones agregar la clase "agua"
-
 */
+const botonesPorClase = document.querySelectorAll(`.btn`);
+boton1.classList.add(`agua`);
+boton2.classList.add(`agua`);
+boton3.classList.add(`agua`);
